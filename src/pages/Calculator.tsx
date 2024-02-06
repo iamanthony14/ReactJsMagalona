1ad4a9af43c910ade9632375c34b05866e428ffa
iamanthony14 <magalona.anthony14@gmail.com>
2024-02-06 16:24:35 +0800

    Updates

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -8,7 +8,7 @@ export default function Calculator() {
   const [input, setInput] = useState("");
 
   const handleClick = (value: string) => {
-    if (input === "Error" || input === "Infinity") {
+    if (input === "Error" || input === "Infinity") { //If the input is "Error" or "Infinity", set the input to the clicked value.
       setInput(value);
     } else {
       // Check if the current input is the result and the clicked value is an operator
@@ -17,18 +17,18 @@ export default function Calculator() {
         if (/[-+*/]$/.test(input)) {
           setInput((prevInput) => prevInput.slice(0, -1) + value); // Replace the last operator
         } else {
-          setInput((prevInput) => prevInput + value);
+          setInput((prevInput) => prevInput + value); //If the last character is not an operator, simply append the clicked operator.
         }
-      } else if (input === "0" && value === "0") {
+      } else if (input === "0" && value === "0") { //If the input is "0" and the clicked value is also "0", do nothing to prevent adding multiple zeros.
         // Do nothing (prevent adding multiple zeros)
-      } else if (/[-+*/]0$/.test(input) && value === "0") {
+      } else if (/[-+*/]0$/.test(input) && value === "0") { //If the last character is an operator and the clicked value is "0", do nothing to prevent adding zero after the operator.
         // Do nothing (prevent adding zero after operator)
-      } else if (/[-+*/]0$/.test(input) && value !== "0") {
+      } else if (/[-+*/]0$/.test(input) && value !== "0") { //If the last character is an operator and the clicked value is not "0", replace the trailing zero with the clicked value.
         // Replace the trailing zero with the clicked value if it's not zero
         setInput((prevInput) => prevInput.slice(0, -1) + value);
-      }  else if (input === "0") {
+      }  else if (input === "0") { //If the input is ONLY "0", replace it with the clicked value.
         setInput(value);
-      } else {
+      } else { //Otherwise, simply append the clicked value to the input.
         setInput((prevInput) => prevInput + value);
       }
     }
